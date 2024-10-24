ef773c717d239fed0e0664c4c6e93cd46d4379a3
gyeon <78517231+gyeon2357@users.noreply.github.com>
2024-10-24 13:56:20 +0900

    Add files via upload

---
+++ assets/js/tag.js
@@ -0,0 +1,30 @@
+// $(".tag__teaware").on("click", function () {
+//   $(".grid-wrap > .grid").load("../", function () {
+//     $("#bag title").remove();
+//     $("#bag header").remove();
+//     $("#bag footer").remove();
+//     $("#bag #site_alarm_slidemenu_container").remove();
+//     $("#bag #mobile_slide_menu_wrap").remove();
+//     $("#bag #mobile_slide_menu").remove();
+//     $("#bag .pc_slide_menu_container").remove();
+//     $("#bag .preview_mode").remove();
+//     $("#bag .member_profile").remove();
+//     $("#bag .modal").remove();
+//     $("#bag iframe").remove();
+//     $("#bag meta").remove();
+//     $("#bag link").remove();
+//     $("#bag style").remove();
+//     $("#bag script").remove();
+//     $("#bag").replaceAll("<!--.*-->", "");
+//   });
+// });
+
+// $(".tag__teaware").on("click", function () {
+//   $.ajax({
+//     url: "workshop-18",
+//     success: function () {
+//       var refine = $(".grid-wrap > .grid").find(".grid__item");
+//       $(".grid-wrap > .grid").html(refine);
+//     },
+//   });
+// });
